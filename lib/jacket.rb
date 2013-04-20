f7c5cb105d4ef2c25d296b36b626b7d51a3aec1c
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2013-04-20 18:25:30 -0400

    Organize into Compass style extension. Add lib and ruby function.

---
+++ lib/jacket.rb
@@ -0,0 +1,30 @@
+# TODO: Remove lib/ when Sass 3.3 is released
+
+Compass::Frameworks.register("jacket", :path => "#{File.dirname(__FILE__)}/..")
+
+module Jacket
+  VERSION = "1.0.0-alpha"
+  DATE = "2013-05-20"
+end
+
+module Sass::Script::Functions
+
+  # Add the Sass list_separator() function until it lands in Sass 3.3
+
+  # Returns the separator of the given list.
+  # If not a list, returns false.
+  #
+  # @example
+  #   list-separator(1px 2px 3px) => 'space'
+  #   list-separator(1px, 2px, 3px) => 'comma'
+  #   list-separator('foo') => 'space'
+  def list_separator(list)
+    if list.class == Sass::Script::List or list.class == Sass::Script::ArgList
+      Sass::Script::String.new(list.separator.to_s)
+    else
+      Sass::Script::String.new('space')
+    end
+  end
+  declare :separator, [:list]
+
+end
